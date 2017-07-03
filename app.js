2d42b288ee5acdbbc1f143f5e307a72d9e6757ef
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-03 10:50:19 +0700

    use jwt

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cookieParser());
+
 app.listen(3000, () => console.log('Server started!'));
 
 app.get('/muave', (req, res) => {
+    res.cookie('daMuaVe', 'true');
     res.send('Ban da mua ve');
 });
 
 app.get('/vaorap', (req, res) => {
-    res.send('Welcome!');
+    if (req.cookies.daMuaVe === 'true') return res.send('Welcome!');
+    res.send('Ban phai mua ve');
 });
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
+const SECRET_KEY = '2her8192ej';
 const app = express();
 const parser = require('body-parser').urlencoded({ extended: false });
 const User = require('./models/User');
 
-app.use(session({
-    secret: 'dkuhs178yefy83uef',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 5000 }
-}));
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
@@ -31,17 +29,22 @@ app.post('/signIn', parser, (req, res) => {
     const user = new User(email, password);
     user.signIn((err, name) => {
         if (err) return res.send('LOI_DANG_NHAP');
-        req.session.daDangNhap = true;
-        req.session.name = name;
-        res.send('DANG_NHAP_THANH_CONG');
+        jwt.sign({ name }, SECRET_KEY, { expiresIn: 30 }, (err, token) => {
+            res.cookie('token', token);
+            res.send('DANG_NHAP_THANH_CONG');
+        });
     });
 });
 
 app.get('/private', (req, res) => {
     //Neu dang nhap roi, hien thi ra 'Hello + name';
     //redirect toi signIn
-    if (!req.session.daDangNhap) return res.redirect('/signIn');
-    res.send('Welcome ' + req.session.name);
+    const { token } = req.cookies;
+    if (!token) return res.redirect('/signIn');
+    jwt.verify(token, SECRET_KEY, (err, object) => {
+        if (err) return res.redirect('/signIn');
+        res.send('Welcome ' + object.name);
+    });
 });
 
 app.listen(3000, () => console.log('server started!'))
